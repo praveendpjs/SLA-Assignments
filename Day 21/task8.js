b924facd9b784281faeb89f7d22bc0700f44fe75
praveendpjs <praveenaishu2012@gmail.com>
2025-11-02 18:55:35 +0530

    day 21 completed

---
+++ Day 21/task8.js
@@ -0,0 +1,11 @@
+let students = [
+    ["Praveen",90,99,100],
+    ["John",90,99,100],
+    ["Bob",90,99,100],
+    ["Dexter",90,99,100],
+    ["Brian",90,99,100],
+]
+for(let i = 0; i <= students.length-1; i++){
+    console.log(students[i]);
+    
+}
---
+++ Day 21/task9.js
@@ -0,0 +1,9 @@
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9,10];
+
+let combined1 = [...arr1, ...arr2]
+
+console.log(combined1);
+
+let combined2 = arr1.concat(arr2);
+console.log(combined2);
