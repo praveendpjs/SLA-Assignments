0d774fc8f8c2f19c86935e4677b97b95f940648f
praveendpjs <praveenaishu2012@gmail.com>
2025-11-02 15:22:32 +0530

    update

---
+++ Day 21/task1.js
@@ -0,0 +1,4 @@
+let color = ["red","green","blue","purple","pink"]
+console.log(color[0]);
+console.log(color[4]);
+
---
+++ Day 21/task2.js
@@ -0,0 +1,12 @@
+let color = ["red","green","blue","purple","pink"]
+color.push("yellow");
+console.log(color);
+
+color.pop();
+console.log(color);
+
+color.shift();
+console.log(color);
+
+color.unshift("red");
+console.log(color);
